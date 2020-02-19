9f63318d1aa1effe3986edfa621c7f1820de1080
Kylian Balan <kylian.balan@epitech.eu>
2020-02-19 15:06:32 +0100

    correct 4081 component name in exceptions

--- src/Components/N4081Component.cpp
+++ src/Components/N4081Component.cpp
@@ -16,6 +16,7 @@ nts::Components::N4081Component::N4081Component()
     std::shared_ptr<IComponent> cAnd2 = std::make_shared<AndComponent>();
     std::shared_ptr<IComponent> cAnd3 = std::make_shared<AndComponent>();
     std::shared_ptr<IComponent> cAnd4 = std::make_shared<AndComponent>();
+
     this->pins[0] = cAnd1->getPin(1);
     this->pins[1] = cAnd1->getPin(2);
     this->pins[2] = cAnd1->getPin(3);
@@ -39,8 +40,8 @@ nts::Components::N4081Component::N4081Component()
 nts::Tristate nts::Components::N4081Component::compute(size_t pin) const
 {
     if (pin == 0 || pin > this->pin_nb)
-        throw nts::Exception::WrongPinException("Given pin is out of range", "FalseComponent");
+        throw nts::Exception::WrongPinException("Given pin is out of range", "N4081Component");
     if (this->pins[pin - 1]->type == PinType::ELECTRICAL)
-        throw nts::Exception::WrongPinException("Pin cannot be computed (electrical)", "FalseComponent");
+        throw nts::Exception::WrongPinException("Pin cannot be computed (electrical)", "N4081Component");
     return this->pins[pin - 1]->compute();
 }
